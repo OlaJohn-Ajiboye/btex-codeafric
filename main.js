f6a14865ce6755ac533a18fc9a56100238e9b74b
AJIBOYE OLASUNKANMI JOHN <33224920+OlaJohn-Ajiboye@users.noreply.github.com>
2018-01-14 20:40:12 +0200

    Added historical Data

--- main.js
+++ main.js
@@ -18,56 +18,75 @@ coinApp.controller('mainController', function($scope, $http) {
       {code: 'GBP', name: 'British Pound'},
       {code: 'NGN',  name: 'Nigerian Naira'},
       {code: 'CNY',  name: 'Chinese Yuan'},
-      {code: 'CAD',  name: 'Canadian Dollars'}
+      {code: 'CAD',  name: 'Canadian Dollars'},
+      
   ];
   
-    $scope.getPrice = function() {
-      console.log("Trying to get price");   
+    $scope.getPrice = function() { 
       $http({
         method: 'GET',
         url: 'https://api.coindesk.com/v1/bpi/currentprice/' + $scope.currency_to_convert_to.code + '.json'
       }).then(function successCallback(response) {
+        
         $scope.rates = [];
           var bpi = response.data.bpi;
           var disclaimer = response.data.disclaimer;
           $scope.disclaimer=disclaimer;
           $scope.rates.push(bpi['USD']);
           $scope.rates.push(bpi[$scope.currency_to_convert_to.code]);
-
+        
+          
           $scope.calculateTotal();
           $scope.calculateUnits();
+          console.log(response)
         }, function errorCallback(response) {
           console.error(response);
         });
     }
-      $scope.calculateTotal = function() {
-        console.log($scope.rates);
+    $scope.getData = function() {   
+      console.log("working")
+      $http({
+        method: 'GET',
+        url: "https://api.coindesk.com/v1/bpi/historical/close.json?start=2017-10-01&end=2018-01-10"
+      }).then(function successCallback(response) {
         
+       $scope.history=response.data.bpi;
+       console.log(response)
+        }, function errorCallback(response) {
+          console.error(response);
+        });
+    }
+
+  
+      $scope.calculateTotal = function() {   
       angular.forEach($scope.rates, function(bt_rate) {
        var total = bt_rate.rate_float * $scope.units;
-        
         if (bt_rate.code == "USD") {
           $scope.total_currency_amount = 0.00;
-          $scope.total_usd_amount = total;      
-        } else {
-            $scope.total_currency_amount = total;
+          $scope.total_usd_amount = total.toFixed(2);      
+        } else {    
+          
+            $scope.total_currency_amount = total.toFixed(2);
             $scope.total_usd_amount = 0.00; 
-        }
+        };
       });
     };
-   
+        
     $scope.calculateUnits = function() {
+      
       angular.forEach($scope.rates, function(bt_rate) {
         if (bt_rate.code == $scope.currency_to_convert_to.code) {
           var units = parseInt($scope.amount) / bt_rate.rate_float;
           if (bt_rate.code == "USD") {
-            $scope.total_usd_units = units;
+            $scope.total_usd_units = units.toFixed(2);
             $scope.total_currency_units = 0.00;
           } else {
-           $scope.total_currency_units = units;
+            
+           $scope.total_currency_units = units.toFixed(2);
             $scope.total_usd_units = 0.00
           }
         }
       });
     };
   });
+ 
